2713e8f3a71deb545dfc9b303a3f1549a013f8a4
JIO <46927978+jiojio0505@users.noreply.github.com>
2026-03-09 18:53:39 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
 import numpy as np
 
 
-seed_dic = {'abu-airport-4': 7105, 'abu-beach-3': 6140, 'abu-urban-2': 9905,
-            'abu-urban-3': 8180, 'abu-urban-4': 2413, 'hydice': 8123}
+seed_dict = {'abu-airport-1': 5130, 'abu-airport-2': 5599, 'abu-airport-3': 1024,
+             'abu-airport-4': 7105, 'abu-beach-1': 1018, 'abu-beach-2': 9126,
+             'abu-beach-3': 6140, 'abu-beach-4': 7507, 'abu-urban-1': 7210,
+             'abu-urban-2': 9905, 'abu-urban-3': 8180, 'abu-urban-4': 2413,
+             'abu-urban-5': 2425, 'cri': 7305, 'hydice': 8123, 'sandiego': 5521}
 
 
 def hyper_norm(data):
@@ -19,3 +22,4 @@ def hyper_norm(data):
     else:
         norm /= (max_val - min_val)
     return norm
+
